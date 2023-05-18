c9c634b92ac49f4ada9149eaffb1db77b1b6adba
maufdev09 <marufdev09@gmail.com>
2023-05-18 14:24:07 +0600

    fire base cofig env secured done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDIHnxXpXjGgGCuShpL2QShzq8ZBRI6JEk",
+  authDomain: "toy-haven-c1ab7.firebaseapp.com",
+  projectId: "toy-haven-c1ab7",
+  storageBucket: "toy-haven-c1ab7.appspot.com",
+  messagingSenderId: "956904385210",
+  appId: "1:956904385210:web:995039305237d3fb653083",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
